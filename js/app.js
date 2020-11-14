0dfae68a433f61d9dd48a6f59f42d076dff36872
Miguel-Pariente <miguelpl32@hotmail.com>
2020-11-14 19:41:20 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+import * as UI from "./interfaz.js";
+import API from "./api.js";
+
+UI.formularioBuscar.addEventListener("submit", buscarCancion);
+
+function buscarCancion(e) {
+  e.preventDefault();
+
+  //Obtener datos del formulario
+  const artista = document.querySelector("#artista").value;
+  const cancion = document.querySelector("#cancion").value;
+
+  if (!artista || !cancion) {
+    //el usuario dejo al menos un campo vacio
+    UI.divMensajes.textContent = "Error... Todos los campos son obligatorios";
+    UI.divMensajes.classList.add("error");
+
+    setTimeout(() => {
+      UI.divMensajes.textContent = "";
+      UI.divMensajes.classList.remove("error");
+    }, 3000);
+
+    return;
+  }
+
+  //Consultar API
+
+  const busqueda = new API(artista, cancion);
+  busqueda.consultarAPI();
+}
